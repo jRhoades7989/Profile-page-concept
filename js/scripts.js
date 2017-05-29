50b528cef87907ae2f3b93c36ac14a72208b7f21
Johnathan R <jrhoades7989@gmail.com>
2017-05-29 09:17:27 -0400

    Apparently i was inside a different git repo...

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+$(window).resize(function() {
+    $('.header-full').css('height', window.innerHeight+'px');
+    $('main').css('margin-top', window.innerHeight + 'px');
+});
+
+//Nav Functions
+var contentSections, navigationItems;
+$(document).ready(function(){
+    contentSections = $('.cd-section');
+    navigationItems = $('#cd-vertical-nav a');
+    updateNavigation();
+    $(window).scroll(function(){updateNavigation();});
+});
+		
+
+
+
+function updateNavigation() {
+    contentSections.each(function(){
+       $this = $(this);
+       var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+       if ( ( $this.offset().top - $(window).height()/4 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/4 > $(window).scrollTop() ) ) {
+          navigationItems.eq(activeSection).addClass('is-selected');
+      }else {
+          navigationItems.eq(activeSection).removeClass('is-selected');
+      }
+   });
+}
+
+
