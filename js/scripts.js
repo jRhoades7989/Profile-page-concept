5aee1e52130544333a69fdead78ddd07b4788f6a
Johnathan Rhoades <jrhoades7989@gmail.com>
2017-06-13 07:50:36 -0400

    Finished form, began footer

--- js/scripts.js
+++ js/scripts.js
@@ -36,5 +36,25 @@ $(document).ready( function () {
             $(this).attr("placeholder", placeholder);
         });
     });
+
+
+    $(document).ready(function(){
+  $("a [href^='#']").on('click', function(event) {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      jQuery('html, body').animate({
+        scrollTop: jQuery(hash).offset().top - 10 + 'px'
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+  });
+});
 });
 
